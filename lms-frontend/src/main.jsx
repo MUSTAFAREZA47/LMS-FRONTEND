38ee968ef9a7d24fa25b86139c282f1377be561a
MUSTAFAREZA47 <mustafareza47@gmail.com>
2024-06-14 09:59:12 +0530

    added most of the dependencies

--- lms-frontend/src/main.jsx
+++ lms-frontend/src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
 )
