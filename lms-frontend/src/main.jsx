7a9756829925579280495e6e6064fdd3cdf68c2e
MUSTAFAREZA47 <mustafareza47@gmail.com>
2024-06-14 11:41:55 +0530

    added eslint configuration

--- lms-frontend/src/main.jsx
+++ lms-frontend/src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <App />
+      <Toaster/>
     </BrowserRouter>
 )
